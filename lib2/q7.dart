2a873f1c56f7f670f4641c1f66f4dab44c948117
Ali <ali5553754@gmail.com>
2025-02-12 07:15:37 +0200

    add homework2

---
+++ lib2/q7.dart
@@ -0,0 +1,17 @@
+// If-Else with Grades
+// Objective: Practice conditional statements with integer values.
+// Instructions:
+// - Create an integer variable marks.
+// - If marks is 70 or higher, print 'Passed', otherwise print 'Failed'.
+
+import 'dart:io';
+
+void main() {
+  print('Enter your mark');
+  int? marks = int.parse(stdin.readLineSync()!);
+  if (marks >= 70) {
+    print('Passed');
+  } else {
+    print('Failed');
+  }
+}
